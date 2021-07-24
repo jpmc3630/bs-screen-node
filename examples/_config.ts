bb581f05922ace695fae6d1b11ebfeb83db4d9cb
James McGlone <jamespmcglone@gmail.com>
2021-07-25 03:58:05 +0800

    Configuring screen

--- examples/_config.ts
+++ examples/_config.ts
@@ -7,13 +7,37 @@ import {
   RuntimeOptions,
 } from '../src';
 
+
+// var cmdArgs = [
+//   '../text-scroller',
+//   '-f../../fonts/nethack16.bdf', 
+//   '--led-chain=8', 
+//   '--led-rows=16', 
+//   '--led-cols=8', 
+//   '--led-multiplexing=18', 
+//   '--led-parallel=2', 
+//   '--led-slowdown-gpio=5', 
+//   '--led-brightness=100', 
+//   '--led-pixel-mapper=Flipper', 
+//   '-s' + speed, 
+//   '-C' + color, 
+//   '-O' + colorOutline, 
+//   '-B' + bgColor, 
+//   '-t' + spacing, 
+//   message
+// ];
+
 export const matrixOptions: MatrixOptions = {
   ...LedMatrix.defaultMatrixOptions(),
-  rows: 32,
-  cols: 64,
-  chainLength: 2,
-  hardwareMapping: GpioMapping.Regular,
-  parallel: 1,
+  rows: 16,
+  cols: 8,
+  chainLength: 8,
+  multiplexing: 18,
+  parallel: 2,
+  brightness: 100,
+  pixelMapperConfig: "Flipper"
+
+  // hardwareMapping: GpioMapping.Regular,
   // pixelMapperConfig: LedMatrixUtils.encodeMappers(
   //   { type: PixelMapperType.Chainlink }
   // ),
@@ -22,5 +46,5 @@ export const matrixOptions: MatrixOptions = {
 
 export const runtimeOptions: RuntimeOptions = {
   ...LedMatrix.defaultRuntimeOptions(),
-  gpioSlowdown: 4,
+  gpioSlowdown: 5,
 };
